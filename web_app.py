d190236b27c71936fce6e1b07d1b31e65ce31fd9
Wissalalouajaji <106196069+Wissalalouajaji@users.noreply.github.com>
2023-05-31 15:10:32 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -79,7 +79,7 @@ def main():
     # creating a button for Prediction
     
     if st.button('Diabetes Test Result'):
-        Detect = Fraud_prediction([Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount]])
+        Detect = Fraud_prediction([Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount])
         
         
     st.success(Detect)
